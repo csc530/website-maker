7b73ae29b1d976823a609de5666fc260c77f0b37
csc530 <cscousin@lakeheadu.ca>
2021-03-21 19:49:30 -0400

    implemented the uses of get variables in signup and signin pages

--- login.php
+++ login.php
@@ -2,18 +2,24 @@
 	$title = 'Login';
 	require_once 'header.php';
 ?>
-    <h1>Login</h1>
-    <form action="register.php" method="post">
-        <fieldset>
-            <label for="email">Email: </label>
-            <input type="email" name="email" id="email" required />
-            <label for="password">Password: </label>
-            <input type="password" name="password" id="password" required />
-        </fieldset>
-        <label hidden>
-            <input hidden name="login" value="true" type="checkbox" checked required disabled />
-        </label>
-        <button type="submit" class="btn btn-primary">login</button>
-        <a href="signup.php" class="btn btn-secondary" ><button type="button">Sign Up</button></a>
-    </form>
-<?php require_once 'footer.php'?>
+	<h1>Login</h1>
+	<form action="register.php" method="post">
+		<fieldset>
+			<label for="email">Email: </label>
+			<input type="email" name="email" id="email" required />
+			<label for="password">Password: </label>
+			<input type="password" name="password" id="password" required />
+			<?php
+				if($_GET['error'])
+					echo '<p class="p-0 alert-danger">' . $_GET['error'] . '</p>'
+			?>
+		</fieldset>
+		<label hidden>
+			<input hidden name="login" value="true" type="checkbox" checked required disabled />
+		</label>
+		<button type="submit" class="btn btn-primary">login</button>
+		<a href="signup.php" class="btn btn-secondary">
+			<button type="button">Sign Up</button>
+		</a>
+	</form>
+<?php require_once 'footer.php' ?>
--- signup.php
+++ signup.php
@@ -1,18 +1,25 @@
 <?php
 	$title = 'Login';
 	require_once 'header.php';
+	$email = $_GET['email'];
 ?>
-    <h1>Login</h1>
-    <form action="register.php" method="post">
-        <fieldset>
-            <label for="email">Email: </label>
-            <input type="email" name="email" id="email" required>
-            <label for="password">Password: </label>
-            <input type="password" name="password" id="password" required>
-            <label for="confirm-password">Confirm password: </label>
-            <input type="password" name="confirm-password" id="confirm-password" required>
-        </fieldset>
-        <button type="submit" class="btn btn-primary">Register</button>
-        <a href="login.php" class="btn btn-secondary" ><button type="button">Login</button></a>
-    </form>
-<?php require_once 'footer.php'?>
+	<h1>Login</h1>
+	<form action="register.php" method="post">
+		<fieldset>
+			<label for="email">Email: </label>
+			<input type="email" name="email" id="email" required <?php echo "value='$email";?> />
+			<label for="password">Password: </label>
+			<input type="password" name="password" id="password" required>
+			<label for="confirm-password">Confirm password: </label>
+			<input type="password" name="confirm-password" id="confirm-password" required>
+			<?php
+				if($_GET['error'])
+					echo '<p class="p-0 alert-danger">' . $_GET['error'] . '</p>'
+			?>
+		</fieldset>
+		<button type="submit" class="btn btn-primary">Register</button>
+		<a href="login.php" class="btn btn-secondary">
+			<button type="button">Login</button>
+		</a>
+	</form>
+<?php require_once 'footer.php' ?>
