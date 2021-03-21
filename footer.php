5a451f33a5ebc2b0bf02f091e15b176e9072a47e
csc530 <cscousin@lakeheadu.ca>
2021-03-21 14:34:35 -0400

    Created website basic layouts

--- footer.php
+++ footer.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-<body>
-<?php
-?>
+<footer class="modal-footer"><small>Some yadda yadda creaative legal jargon or whatever</small></footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,3 +2,8 @@
     $title = 'C-Builder';
     require_once 'header.php';
 ?>
+<h1>Site Name</h1>
+<p>Intoductory jargon about website and capabilities.</p>
+<form action="hosted-sites.php" method="get">
+<button type="button">View Sites</button>
+</form>
