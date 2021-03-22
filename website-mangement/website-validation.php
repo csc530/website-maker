75b7e173251deae75bba60d2cdf297b1e302ae6b
csc530 <cscousin@lakeheadu.ca>
2021-03-22 17:28:00 -0400

    added validation for title and description to save initial basic website to database

---
+++ website-mangement/website-validation.php
@@ -0,0 +1,34 @@
+<?php
+	$step = $_POST['step'];
+	$error = 'Please try again';
+	//check what step of the website building process user is on
+	if($step==1)
+	{
+		$title = $_POST['title'];
+		$description = $_POST['description'];
+		if(empty($title))
+			$error = 'Website title cannot be empty';
+		else if(empty($description))
+			$error = 'Website description cannot be empty';
+		else
+		{
+			try
+			{
+				$error = 'Network error, please try again.';
+				require_once 'connect.php';
+				$sql = 'INSERT INTO websites (creator,name, description) VALUES (:email,:title,:desc);';
+				$cmd = $db->prepare($sql);
+				//todo:get email from session
+				$cmd->bindParam(':email' , $email, PDO::PARAM_STR, 128);
+				$cmd->bindParam(':title', $title, PDO::PARAM_STR, 35);
+				$cmd->bindParam(':desc', $description, PDO::PARAM_STR, 600);
+				$cmd->execute();
+				header("location:content.php?title=$error");
+			}
+			catch(Exception $exception)
+			{
+				header("location:create.php?error=$error");
+			}
+		}
+	}
+	?>
