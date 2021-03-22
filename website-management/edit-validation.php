3ddd1ca6c8b351a8e3fb26c791615b618ea14cab
csc530 <cscousin@lakeheadu.ca>
2021-03-22 19:20:21 -0400

    added validation for adding a new user to admin for a website

--- website-management/edit-validation.php
+++ website-management/edit-validation.php
@@ -1,21 +1,48 @@
 <?php
-	$newUser = $_POST['user'];
+	$error = 'Please try again.';
+	//edit button to edit additional page content
 	$editPage = $_POST['edit'];
+	//update button if they updated the title or description
 	$basic = $_POST['update'];
-	$websiteID = $_POST['websiteID'];
+	//add button to add a new admin user
 	$addUser = $_POST['add'];
+	//new user email account
+	$newUser = $_POST['user'];
+	//website's ID
+	$websiteID = $_POST['websiteID'];
 	if($editPage == 'true')
 		header("location:edit-page.php?pageID=$websiteID");
 	else if(!empty($addUser))
 	{
-		require_once '../connect.php';
-		$sql = 'INSERT INTO websites_admin VALUES (:websiteID, :user);';
-		$cmd = $db->prepare($sql);
-		$cmd->bindParam(':websiteID',$websiteID,PDO::PARAM_INT, 11);
-		//db side checking as user is a foreign key so that user must exist
-		$cmd->bindParam(':user', $newUser, PDO::PARAM_STR, 128);
-		$cmd->execute();
-		//todo ui: pass get variable to allow the new user to be highlighted for a time or something
-		header("location:edit.php");
+		try
+		{
+			$error='Network error please try again.';
+			require_once '../connect.php';
+			$sql = 'SELECT email FROM users WHERE email = :user';
+			$cmd = $db->prepare($sql);
+			$cmd->execute();
+			$user = $cmd->fetch();
+			if($user['email'] == $newUser)
+			{
+				$sql = 'INSERT INTO websites_admin VALUES (:websiteID, :user);';
+				$cmd = $db->prepare($sql);
+				$cmd->bindParam(':websiteID', $websiteID, PDO::PARAM_INT, 11);
+				//db side checking as user is a foreign key so that user must exist
+				$cmd->bindParam(':user', $newUser, PDO::PARAM_STR, 128);
+				$cmd->execute();
+				//todo ui: pass get variable to allow the new user to be highlighted for a time or something
+				header("location:edit.php");
+			}
+			else
+			{
+				$error = "There is no registered user with $newUser email address. <a href='../signup.php?email=$newUser'>register</a> with it now.";
+				header("location:edit.php?error=$error");
+			}
+		}
+		catch(Exception $exception)
+		{
+			header("location:edit.php?error=$error");
+		}
 	}
+	else if(!empty())
 		?>
