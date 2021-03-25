ae492595b3948a8ab230880c0e2320d3c207483b
csc530 <cscousin@lakeheadu.ca>
2021-03-25 12:29:51 -0400

    fixed broken links in logged out nav

--- main-header.php
+++ main-header.php
@@ -1,14 +1,14 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="../pages/index.php">Site name</a>
+		<a class="navbar-brand" href="index.php">Site name</a>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link" href="../pages/signup.php">Register</a>
+					<a class="nav-link" href="signup.php">Register</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="../pages/login.php" role="button">Login</a>
+					<a class="nav-link" href="login.php" role="button">Login</a>
 				</li>
 			</ul>
 			<form class="d-flex">
