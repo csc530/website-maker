cf62212678f52b804ffcf578c187793e7e9947a8
csc530 <cscousin@lakeheadu.ca>
2021-03-25 14:04:11 -0400

    started adding functionality to add webpage's content and title to db associated with the right website

--- edit-webpages.php
+++ edit-webpages.php
@@ -10,34 +10,61 @@
 	if(empty($pageNumber) || !is_numeric($pageNumber) || $pageNumber < 1)
 		$pageNumber = 1;
 	require_once 'connect.php';
-	//create specified number of pages of they are creating a new website.
-	//Or get pages from database if they are editing an existing one
+	//if the webpage's website ID is not given in the url get it from the db
 	if(empty($webID))
+		//get the websites ID from db
+		//this will work (with fetch) as each user must have a unique website name for each of their sites
+		$sql = 'SELECT ID FROM websites WHERE name = :title AND creator = :email;';
+	$cmd = $db->prepare($sql);
+	$cmd->bindParam(':title', $title, PDO::PARAM_STR, 35);
+	$cmd->bindParam(':email', $_SESSION['email'], PDO::PARAM_STR, 128);
+	$cmd->execute();
+	$webID = $cmd->fetch();
+	$webID = $webID['ID'];
+	try
 	{
-		try
-		{
-			$sql = 'SELECT title, content FROM pages WHERE websiteID = :webID AND pageNumber = :pageNum;';
-			$cmd = $db->prepare($sql);
-			$cmd->bindParam(':webID', $webID, PDO::PARAM_INT, 11);
-			$cmd->bindParam(':pageNum', $pageNumber, PDO::PARAM_INT, 11);
-			$cmd->execute();
-			$pageDetails = $cmd->fetch();
-		}
-		catch(Exception $exception){}
+		$sql = 'SELECT title, content FROM pages WHERE websiteID = :webID AND pageNumber = :pageNum;';
+		$cmd = $db->prepare($sql);
+		$cmd->bindParam(':webID', $webID, PDO::PARAM_INT, 11);
+		$cmd->bindParam(':pageNum', $pageNumber, PDO::PARAM_INT, 11);
+		$cmd->execute();
+		$pageDetails = $cmd->fetch();
+	}
+	catch(Exception $exception)
+	{
+	}
+	
+	try
+	{
+		$sql = 'SELECT pageNumber, websiteID, title FROM pages WHERE websiteID = :webID AND pageNumber != :pageNum;';
+		$cmd = $db->prepare($sql);
+		$cmd->bindParam(':webID', $webID, PDO::PARAM_INT, 11);
+		$cmd->bindParam(':pageNum', $pageNumber, PDO::PARAM_INT, 11);
+		$cmd->execute();
+		$pages = $cmd->fetchAll();
+		if(!empty($pages))
+			echo "<h2>pages</h2>\n<ul>";
+		foreach($pages as $page)
+			echo '<li><a href="edit-webpages.php?pageNumber=' . $page['pageNumber'] . '&webID=' . $page['websiteID'] . '">' . $page['title'] . '</a></li>';
+		if(empty($pages))
+			echo "</ul>";
+	}
+	catch(Exception $exception)
+	{
 	}
 ?>
-	<form action="website-validation.php?pageNumber="<?php echo "$pageNumber"; ?> method="post">
-		<h1><?php echo "$title: page $pageNumber" ?></h1>
-		<label for="pageTitle">Page title</label>
-		<input id="pageTitle" name="pageTitle" type="text" required max="50" min="1"
-		       value="<?php if(!empty($pageDetails)) echo $pageDetails['title']; ?>" />
-		<label for="pageContent">Page content</label>
-		<textarea name="pageContent" id="pageContent" required
-		          placeholder="HTML content is allowed i.e. <h1>Hello</h1><p>Welcome to my page, I hope you enjoy</p>..."><?php
-				if(!empty($pageDetails))
-					echo $pageDetails['content'];
-			?></textarea>
-		<button type="submit">Submit</button>
+	<form action="website-validation.php?pageNumber=<?php echo "$pageNumber&webID=$webID&pageTitle=$title"; ?>" method="post">
+	<h1><?php echo "$title: page $pageNumber" ?></h1>
+	<label for="pageTitle">Page title</label>
+	<input id="pageTitle" name="pageTitle" type="text" required max="50" min="1"
+	       value="<?php if(!empty($pageDetails)) echo $pageDetails['title']; ?>" />
+	<label for="pageContent">Page content</label>
+	<textarea name="pageContent" id="pageContent" required
+	          placeholder="HTML content is allowed i.e. <h1>Hello</h1><p>Welcome to my page, I hope you enjoy</p>..."><?php
+			if(!empty($pageDetails))
+				echo $pageDetails['content'];
+		?></textarea>
+	<button type="submit" name="step" value="2">Submit</button>
 	</form>
 <?php
 	require_once 'footer.php';
--- website-validation.php
+++ website-validation.php
@@ -6,8 +6,8 @@
 	//check what step of the website building process user is on
 	if($step==1)
 	{
-		$title = $_POST['title'];
-		$description = $_POST['description'];
+		$title = trim($_POST['title']);
+		$description = trim($_POST['description']);
 		if(empty($title))
 			$error = 'Website title cannot be empty';
 		else if(empty($description))
@@ -25,7 +25,42 @@
 				$cmd->bindParam(':title', $title, PDO::PARAM_STR, 35);
 				$cmd->bindParam(':desc', $description, PDO::PARAM_STR, 600);
 				$cmd->execute();
-				header("location:edit-webpages.php?title=$title");
+				header("location:edit-webpages.php?title=$title&pageNumber=1");
+			}
+			catch(Exception $exception)
+			{
+				header("location:create.php?error=$error");
+			}
+		}
+	}
+	else if($step==2)
+	{
+		$pageNumber = $_GET['pageNumber'];
+		$pageTitle = trim($_POST['pageTitle']);
+		$title = trim($_GET['title']);
+		$content = trim($_POST['pageContent']);
+		$webID = $_GET['webID'];
+		if(empty($title))
+			$error = 'Page title cannot be empty';
+		else if(empty($description))
+			$error = 'Page content cannot be empty';
+		else
+		{		echo "bepp";
+			
+			try
+			{
+				$error = 'Network error, please try again.';
+				require_once 'connect.php';
+				$sql = 'INSERT INTO pages VALUES (:title,:content,:webID,:pgNum);';
+				$cmd = $db->prepare($sql);
+				$cmd->bindParam(':title' , $title, PDO::PARAM_STR, 50);
+				$cmd->bindParam(':content', $content, PDO::PARAM_STR, 10000);
+				$cmd->bindParam(':webID', $webID, PDO::PARAM_INT, 11);
+				$cmd->bindParam(':pgNum',$pageNumber, PDO::PARAM_INT, 11);
+				$cmd->execute();
+				//increment the page number as to 'add' a new page
+				$pageNumber++;
+				header("location:edit-webpages.php?title=$title&pageNumber=$pageNumber&webID=$webID");
 			}
 			catch(Exception $exception)
 			{
