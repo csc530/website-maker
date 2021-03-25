221a1a78c4a5a70fec970c2f390ccb2083eb32d8
csc530 <cscousin@lakeheadu.ca>
2021-03-25 12:11:31 -0400

    fixed broken logged in user nav

--- user-header.php
+++ user-header.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="../pages/index.php">Site name</a>
+		<a class="navbar-brand" href="index.php">Site name</a>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link" href="../pages/profile.php"><?php echo $_SESSION['email'];?></a>
+					<a class="nav-link" href="profile.php"><?php echo $_SESSION['email'];?></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="../pages/webmaker/menu.php" role="button">My sites</a>
+					<a class="nav-link" href="menu.php" role="button">My sites</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="../pages/logout.php" role="button">Logout</a>
+					<a class="nav-link" href="logout.php" role="button">Logout</a>
 				</li>
 			</ul>
 			<form class="d-flex">
