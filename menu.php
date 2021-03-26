b27fb3d14db2206eae98a87d5d2b45146883427c
csc530 <cscousin@lakeheadu.ca>
2021-03-25 21:18:36 -0400

    added pre-existing self created websites in user menu

--- menu.php
+++ menu.php
@@ -9,9 +9,7 @@
 		<?php
 		//todo populate ul with website hrefs from database
 			require_once 'connect.php';
-			$sql = 'SELECT name, preview, addr FROM websites
-    				INNER JOIN websites_admin ON websites.ID = websites_admin.websiteID
-					WHERE admin=:email;';
+			$sql = 'SELECT name FROM websites WHERE creator=:email;';
 			$cmd = $db->prepare($sql);
 			$email = $_SESSION['email'];
 $cmd->bindParam(':email',$email, PDO::PARAM_STR, 128);
@@ -19,7 +17,26 @@ $cmd->bindParam(':email',$email, PDO::PARAM_STR, 128);
 			$db=null;
 			$websites = $cmd->fetchAll();
 			foreach($websites as $site)
-				echo '<li><a href="'.$site['addr'].'">'.$site['preview'].'</a></li>';
+				echo '<li><a href="#">'.$site['name'].'</a></li>';
+		?>
+		<li><a href="create.php">+</a></li>
+	</ul>
+	<h2>Collaborating sites</h2>
+	<ul>
+		<?php
+			//todo populate ul with website hrefs from database
+			require_once 'connect.php';
+			$sql = 'SELECT name FROM websites
+    				INNER JOIN websites_admin ON websites.name = websites_admin.siteName
+					WHERE admin=:email;';
+			$cmd = $db->prepare($sql);
+			$email = $_SESSION['email'];
+			$cmd->bindParam(':email',$email, PDO::PARAM_STR, 128);
+			$cmd->execute();
+			$db=null;
+			$websites = $cmd->fetchAll();
+			foreach($websites as $site)
+				echo '<li><a href="'.$site['addr'].'">'.$site['name'].'</a></li>';
 		?>
 		<li><a href="create.php">+</a></li>
 	</ul>
