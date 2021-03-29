3bf3760209b5bca3ba504a5ec560cf49498d7c08
csc530 <cscousin@lakeheadu.ca>
2021-03-29 10:56:25 -0400

    added canEdit.php to check if user has permission to edit webpage

--- publish.php
+++ publish.php
@@ -3,20 +3,36 @@
 	require_once 'authenticate.php';
 	$title = "Finalize";
 	$siteName = $_GET['siteTitle'];
+	$creator = $_GET['creator']
 	require_once 'meta.php';
 	?>
-<form action="website-validation.php?siteTitle=<?php echo "$siteName"; ?>" method="post">
-		<h1>Review</h1>
+<form action="website-validation.php?siteTitle=<?php
+	echo "$siteName"; ?>" method="post">
+	<h1>Review</h1>
 	<h2>Website Overview</h2>
 	<!--todo add mini view of pages with link to edit them and special link for splash back to website edit not page-->
-	<h3>Website name: <?php echo $siteName; ?></h3>
+	<h3>Website name: <?php
+			echo $siteName; ?></h3>
 	<h3>Website overview:</h3>
 	<?php
-	
+		require_once 'connect.php';
+		//get description opf website and total number of pages
+		$sql = 'SELECT description, COUNT(pageNumber) AS `pages` FROM websites INNER JOIN pages p on p.siteName = :siteName WHERE p.creator = :creator GROUP BY description;';
+		$cmd = $db->prepare($sql);
+		$cmd->bindParam(':creator', $creator, PDO::PARAM_STR, 128);
+		$cmd->bindParam(':siteName', $siteName, PDO::PARAM_STR, 35);
+		$cmd->execute();
+		$pageInfo = $cmd->fetch();
+		//display website's description
+		echo '<p>' . $pageInfo['description'] . '</p>';
+		//display total number of pages
+		echo '<h3>Page count: ' . $pageInfo['pages'] . '</h3>';
 	?>
+	<!--
 <label>Colour theme
-	<!--Add option to personalize website theme-->
+	Add option to personalize website theme
 </label>
+-->
 	<!--todo: add an onclick to button view published site or return home-->
 	<button type="submit" name="step" value="4">Finalize</button>
 </form>
--- website-validation.php
+++ website-validation.php
@@ -10,13 +10,17 @@
 	//if user is coming from create.php (creating website title and description)
 	if($step==1)
 	{
-		//
+		//trim whitespace from website title and description
 		$siteName = trim($_POST['title']);
 		$description = trim($_POST['description']);
+		//basic validation of website name and description
 		if(empty($siteName))
 			$error = 'Website title cannot be empty';
 		else if(empty($description))
-			$error = 'Website description cannot be empty';
+			$error = 'Website overview cannot be empty';
+		//I choose for 'Hi' as a basic and simplest overview
+		else if(strlen($description)<2)
+			$error = 'Website overview must at least be 2 characters';
 		else
 		{
 			try
@@ -59,12 +63,14 @@
 			}
 		}
 	}
+	//check for individual website's pages
 	else if($step==2)
 	{
 		$pageNumber = $_GET['pageNumber'];
 		$pageTitle = trim($_POST['pageTitle']);
 		$siteName = $_GET['siteTitle'];
 		$content = trim($_POST['pageContent']);
+		//validate page title and content
 		if(empty($pageTitle))
 			$error = 'Page title cannot be empty';
 		else if(empty($content))
@@ -73,6 +79,8 @@
 		{
 			try
 			{
+				//verify user can edit this page
+				require_once 'canEdit.php';
 				$error = 'Network error, please try again.';
 				require_once 'connect.php';
 				//check if there if they are updating an existing page
@@ -83,7 +91,8 @@
 				$cmd->bindParam(':siteName',$siteName,PDO::PARAM_STR, 35);
 				$cmd->execute();
 				$isUpdate = $cmd->fetch();
-				//if the fetch is empty then there is no existing page at that number so insert into db if not update that pageNumber
+				//if the fetch is empty then there is no existing page at that number so insert into db if not update (page of that pageNumber)
+				//if statement changing SQL query as (you can see) the following code is the same
 				if(empty($isUpdate))
 					$sql = 'INSERT INTO pages VALUES (:name,:siteName,:creator,:content,:pageNumber);';
 				else
@@ -95,7 +104,7 @@
 				$cmd->bindParam(':pageNumber', $pageNumber, PDO::PARAM_INT, 11);
 				$cmd->bindParam(':siteName', $siteName, PDO::PARAM_STR, 35);
 				$cmd->execute();
-				//increment the page number as to 'add' a new page
+				//increment the page number as to 'add' a new page (or progress to new page if they updated)
 				$pageNumber= $pageNumber+1;
 				header("location:edit-webpages.php?siteTitle=$siteName&pageNumber=$pageNumber&creator=$creator");
 				exit();
@@ -115,19 +124,22 @@
 				exit();
 			}
 		}
+		//redirect for invalid page title or content
 		header("location:edit-webpages.php?siteTitle=$siteName&creator=$creator&pageNumber=$pageNumber&pageTitle=$pageTitle&content=$content&error=$error&step=$step");
 		exit();
 	}
+	//submission of website (review)
 	else if($step==3)
 	{
 		$siteName = $_GET['siteTitle'];
+		//verify user can edit this page
+		require_once 'canEdit.php';
 		header("location:publish.php?siteTitle=$siteName&creator=$creator");
 		exit();
 	}
 	else
 	{
-		//todo: add appropriate redirect when a site page is clicked to edit it
-		//todo: update and add publish column to site allow viewing it in when opened
+		//if they messed with the form and edited the step redirect to home page
 		header('location:menu.php');
 		exit();
 	}
