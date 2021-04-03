eca5fdc7a52033542bc3995534c2b4ccfbe3080c
csc530 <cscousin@lakeheadu.ca>
2021-04-02 20:44:59 -0400

    added all visual styles/cues for admin tasks edit, add, and delete users

--- pages/super-user.php
+++ pages/super-user.php
@@ -15,10 +15,55 @@
 		<input type="hidden" name="superuser" value="true" />
 		<button type="submit">Submit</button>
 	</form>
-<?php
-
-?>
+	<form action="super-user.php" method="post">
+		<label for="editUser">Change user email: </label>
+		<select name="editUser" id="editUser" required>
+			<?php
+				
+				require_once 'connect.php';
+				$alph = 'ABCDEFGHILKMNOPQRSTUVWXYZ';
+				for($i = 0; $i < 26; $i++)
+				{
+					
+					$letter = $alph[$i];
+					echo "<optgroup label='$letter'>";
+					//selects email from registered based if the first character mathces position in alpahbet in looop
+					$sql = 'SELECT email AS email FROM creators WHERE SUBSTRING(email,1,1) = :letter';
+					
+					try
+					{
+						$cmd = $db->prepare($sql);
+						$cmd->bindParam(':letter', $letter, PDO::PARAM_STR, 1);
+						$cmd->execute();
+						$users = $cmd->fetchAll();
+					}
+					catch(Exception $ex){
+					}
+					foreach($users as $user)
+						if(!empty($user['email']))
+						echo '<option>'.$user['email'].'</option>';
+						echo '</optgroup>';
+				}
+				$db=null;
+			?>
+		</select>
+		<label for="newEmail">New email: </label>
+		<input type="email" name="newEmail" id="newEmail"/>
+		<button type="submit" class="btn btn-danger">Change</button>
 	</form>
+	<h2>Delete users: </h2>
+	<ul>
+		<?php
+			require 'connect.php';
+			$sql='SELECT email FROM creators';
+			$cmd = $db->prepare($sql);
+			$cmd->execute();
+			$users = $cmd->fetchAll();
+			foreach($users as $user)
+				if(!empty($user['email']))
+					echo '<li><a href="delete.php?user='.$user['email'].'"  onclick="return confirmDelete()" >'.$user['email'].'<button type="button" class="btn-danger">-</button></a></li>';
+		?>
+	</ul>
 <?php
 	require_once 'footer.php';
 ?>
