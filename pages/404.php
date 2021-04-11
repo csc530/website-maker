f787993322f1d077bb4d171f5d9b72ee516badf3
csc530 <cscousin@lakeheadu.ca>
2021-04-11 17:47:43 -0400

    Fixed htaccess 404 redirect

--- pages/404.php
+++ pages/404.php
@@ -1,10 +1,10 @@
 <?php
 	require_once 'meta.php';
 	?>
-	<div class="alert alert-dismissible">
+	<div class="alert alert-info">
 		<h1>Page not found</h1>
 		<p>Sorry looks like the page your looking for doesn't exist. Please check your URL and try again.</p>
-		<a href="index.php"><button class="btn-close-white">Home</button></a>
+		<a href="index.php"><button class="btn-primary">Home</button></a>
 	</div>
 <?php
 	require 'footer.php'
